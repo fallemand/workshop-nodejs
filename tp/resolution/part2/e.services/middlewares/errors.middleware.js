cb2008dc5ef16f220ccbdf6871964093bb26efbd
Facundo Allemand <facundo.allemand@mercadolibre.com>
2017-08-22 17:01:14 -0300

    fix errors middleware

--- tp/resolution/part2/e.services/middlewares/errors.middleware.js
+++ tp/resolution/part2/e.services/middlewares/errors.middleware.js
@@ -1,7 +1,15 @@
+//Log error function
+const logError = (err) => {
+    console.error(err);
+}
+
 exports.apiErrors = (err, req, res, next) => {
-    res.status(err.status || 500).json({error: err.message});
+    logError(err);
+    res.status(err.status || 500).json({error: err});
 }
 
 exports.appErrors = (err, req, res, next) => {
+    logError(err);
     res.render('error', {message : JSON.stringify(err, null, 4)});
 }
+
--- tp/resolution/part4/middlewares/errors.middleware.js
+++ tp/resolution/part4/middlewares/errors.middleware.js
@@ -1,13 +1,14 @@
-exports.apiErrors = (err, req, res, next) => {
+//Log error function
+const logError = (err) => {
     console.error(err);
+}
+
+exports.apiErrors = (err, req, res, next) => {
+    logError(err);
     res.status(err.status || 500).json({error: err});
 }
 
 exports.appErrors = (err, req, res, next) => {
-    console.error(err);
+    logError(err);
     res.render('error', {message : JSON.stringify(err, null, 4)});
-}
-
-const logError = (err) => {
-
 }
