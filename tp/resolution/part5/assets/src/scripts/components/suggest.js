7a1ca7d200812ce6d3f437714955c351e3bc4d1b
Facundo Allemand <facundo.allemand@mercadolibre.com>
2017-09-04 09:53:02 -0300

    fix var / const variable definition.

--- tp/resolution/part5/assets/src/scripts/components/suggest.js
+++ tp/resolution/part5/assets/src/scripts/components/suggest.js
@@ -1,22 +1,22 @@
-var request = require('./request.js');
-var template = require('../../../../views/partials/suggest.hbs');
+const request = require('./request.js');
+const template = require('../../../../views/partials/suggest.hbs');
 
 module.exports = (input, container) => {
 
     //Define Callbacks
     //-------------------------------------------------------------
-    var success = (data) => {
+    const success = (data) => {
         container.innerHTML = template(JSON.parse(data));
         body.addEventListener('click', hideMenuListener);
     }
-    var err = (err) => {
+    const err = (err) => {
         console.log(err);
     }
 
     //Define Events
     //-------------------------------------------------------------
-    var body = document.body;
-    var hideMenuListener = function (event) {
+    const body = document.body;
+    const hideMenuListener = function (event) {
         container.innerHTML = '';
         body.removeEventListener('click', hideMenuListener);
     };
@@ -33,7 +33,7 @@ module.exports = (input, container) => {
     //Suggest on every key
     input.addEventListener('keyup', () => {
         if(input.value.length > 2) {
-            var url = '/api/suggest?q=' + input.value + '&v=1';
+            const url = '/api/suggest?q=' + input.value + '&v=1';
             request(url, success, err);
         }
     })
