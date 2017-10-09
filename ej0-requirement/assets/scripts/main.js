3ea2b459f1fa4e1b19917429e1fc5ac4b33161e7
abrana <agustin.brana@mercadolibre.com>
2017-10-08 22:19:53 -0300

    Ejercicio 0

--- ej0-requirement/assets/scripts/main.js
+++ ej0-requirement/assets/scripts/main.js
@@ -1,5 +1,39 @@
 window.onload = function() {
+    let searchUrl = 'https://api.mercadolibre.com/sites/MLA/search?q=';
+    const form = document.querySelector("[data-js=form]");
+    let inputEntry = document.querySelector("[data-js=search]")
+    
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        searchUrl += inputEntry.value;
+        request(searchUrl, listResult, logError);
+    });
+}
 
+function listResult(data) {
+    data = JSON.parse(data);
+    
+    const breadcrumbContainer = document.querySelector("[data-js=breadcrumb-container]");
+    const breadcrumbList = document.getElementById("breadcrumb");
+    const breadcrumbTemplate = Handlebars.compile(breadcrumbList.innerHTML);
+
+    const mainContent = document.querySelector("[data-js=main-content]");
+    const productList = document.getElementById("product-list");
+    const productTemplate = Handlebars.compile(productList.innerHTML);
+    let productModel = {};
+    let breadcrumbModel = {};
+ 
+    if (data.results.length > 0) {
+        breadcrumbModel = { breadCrumbList: data.filters[0].values[0].path_from_root };
+        productModel = { products: data.results };
+    }
+
+    breadcrumbContainer.innerHTML = breadcrumbTemplate(breadcrumbModel);
+    mainContent.innerHTML = productTemplate(productModel);
+}
+
+function logError(error) {
+    console.log("Error: ", error);
 }
 
 //Use this method to make request to the API
