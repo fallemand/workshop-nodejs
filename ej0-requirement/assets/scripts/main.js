e864100d029374af72f941bad5df277797410655
jessipedernera <jessica.pedernera@mercadolibre.com>
2017-10-09 02:30:54 -0300

    query and bradcrumb

--- ej0-requirement/assets/scripts/main.js
+++ ej0-requirement/assets/scripts/main.js
@@ -1,10 +1,47 @@
 window.onload = function() {
+    let apiUrl = 'https://api.mercadolibre.com/sites/MLA/search?q=';
+    let searchForm = document.querySelector('[data-js="form"]');
+    let searchInput = document.querySelector('[data-js="search"]');
 
+    searchForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        apiUrl += searchInput.value;
+        request(apiUrl, productsList, errorCall)
+    });
+};
+
+function productsList(data) {
+    data = JSON.parse(data);
+    console.log(data);
+    //searchInput.value = '';
+
+    const breadcrumbContainer = document.querySelector("[data-js=breadcrumb-container]");
+    const breadcrumbTemplate = document.getElementById("breadcrumb-template");
+    const pTemplate = Handlebars.compile(breadcrumbTemplate.innerHTML);
+
+    const searchContent = document.querySelector("[data-js=main-content]");
+    const searchTemplate = document.getElementById("search-template");
+    const sTemplate = Handlebars.compile(searchTemplate.innerHTML);
+
+    let searchItems = {};
+    let listItems = {};
+
+    if (data.results.length > 0) {
+        listItems = { listItems: data.filters[0].values[0].path_from_root };
+        searchItems = { searchItems: data.results };
+    }
+
+    breadcrumbContainer.innerHTML = pTemplate(listItems);
+    searchContent.innerHTML = sTemplate(searchItems);
+}
+
+function errorCall(error) {
+    console.log('Hubo un error: '+error);
 }
 
 //Use this method to make request to the API
 function request(url, callback, error) {
-    var xhr = new XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
     xhr.open("GET", url, true);
     xhr.onload = function(e) {
         if (xhr.readyState === 4) {
