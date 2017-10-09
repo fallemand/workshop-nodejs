a3b8377da54e13d498119aa4357a056d68a894e7
Hernan Garzón <hernan.garzon@mercadolibre.com>
2017-10-09 09:33:58 -0300

    add ej0 changes

--- ej0-requirement/assets/scripts/main.js
+++ ej0-requirement/assets/scripts/main.js
@@ -1,6 +1,25 @@
 window.onload = function() {
+    document.getElementsByClassName('header__search-input')[0].addEventListener('change', function (e) {
+        var text = this.value;
+        request('https://api.mercadolibre.com/sites/MLA/search?q=' + text, 
+            function(data) {
 
-}
+                // https://tutorialzine.com/2015/01/learn-handlebars-in-10-minutes
+
+                var source   = document.getElementById("entry-template").innerHTML;
+                var template = Handlebars.compile(source);
+                var compiledHtml    = template(JSON.parse(data));
+
+                console.log(compiledHtml);
+
+                document.getElementsByClassName('main__content')[0].innerHTML = compiledHtml;
+            }, 
+            function(e) {
+                alert(e);
+            }
+        );
+    });
+};
 
 //Use this method to make request to the API
 function request(url, callback, error) {
