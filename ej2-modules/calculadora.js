b88c79726b4cd1c30d05975e568659beb355fa12
Juan Manuel Morazano <juan.morazano@mercadolibre.com>
2017-10-09 12:31:57 -0300

    ej2

---
+++ ej2-modules/calculadora.js
@@ -0,0 +1,13 @@
+exports.sumar = (a, b) => a + b
+
+exports.restar = (a, b) => {
+    return a - b;
+}
+
+exports.multiplicar = (a, b) => {
+    return a * b;
+}
+
+exports.dividir = (a, b) => {
+    return a / b;
+}
---
+++ ej2-modules/index.js
@@ -0,0 +1,6 @@
+const calculadora = require('./calculadora.js');
+
+console.log('Suma 3 + 5 = ', calculadora.sumar(3, 5));
+console.log('Resta 3 - 5 = ', calculadora.restar(3, 5));
+console.log('Divide 3 ÷ 5 = ', calculadora.dividir(3, 5));
+console.log('Multiplica 3 x 5 = ', calculadora.multiplicar(3, 5));
