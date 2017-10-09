a088c705ad28842ca601a96436e5f0e8773c4d27
rodracastro <rodrigo.castro@mercadolibre.com>
2017-10-08 23:34:26 -0300

    Retrieving items based on search and rendering result view

--- ej0-requirement/assets/scripts/main.js
+++ ej0-requirement/assets/scripts/main.js
@@ -1,6 +1,10 @@
 window.onload = function() {
-
-}
+    document.getElementsByClassName('header__search')[0].addEventListener('submit', function(e) {
+      e.preventDefault();
+        var search = document.getElementsByClassName('header__search-input')[0].value;
+        request('https://api.mercadolibre.com/sites/MLA/search?q=' + search, renderView, error);
+    });
+};
 
 //Use this method to make request to the API
 function request(url, callback, error) {
@@ -20,3 +24,23 @@ function request(url, callback, error) {
     };
     xhr.send(null);
 }
+
+var renderView = function (data) {
+    var source = document.getElementById("entry-template").innerHTML;
+    var template = Handlebars.compile(source);
+    var html = template(JSON.parse(data));
+    document.getElementsByClassName("main")[0].innerHTML = html;
+};
+
+var error = function (error) {
+    alert(error);
+};
+
+Handlebars.registerHelper('formatPrice', function(price) {
+    price = price.toLocaleString('de-DE');
+    var splittedPrice = price.split(",");
+    price = splittedPrice[0] ? splittedPrice[0] : price;
+    var cents = splittedPrice[1] ? splittedPrice[1] : '00';
+    price = new Handlebars.SafeString('\$' + price + '<sup>'+cents+'</sup>');
+    return price;
+});
