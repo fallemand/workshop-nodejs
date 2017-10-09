2f7bb9f359cdcbba06449dbce2dc9a82398acc60
Gonzalo E. Rodriguez Isleño <gonzalo.rodriguez@mercadolibre.com>
2017-10-09 00:33:37 -0300

    Requirement exercise finished.

--- ej0-requirement/assets/scripts/main.js
+++ ej0-requirement/assets/scripts/main.js
@@ -1,5 +1,50 @@
 window.onload = function() {
+    Handlebars.registerHelper('formatMoney', function(options) {
+        //TODO: Ver una forma de independizar el formateo de la localización.
+        var formattedMoney = options.hash.amount.toLocaleString( options.hash.locale,
+            {
+                style: 'currency',
+                currency: options.hash.currency_id,
+                currencyDisplay: 'symbol',
+                useGrouping: options.hash.grouping,
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2
+            });
+        return new Handlebars.SafeString(
+            '<span class="currency-symbol">' + formattedMoney[0] + '</span>' +
+            '<span class="amount">' + formattedMoney.slice(1,-3) + '</span>' +
+            '<sup class="fractional">' + formattedMoney.substr(-2,2) + '</sup>'
+        );
+    });
+    var form = document.querySelector('[data-js="form"]');
+    var input = form.querySelector('[data-js="search"]');
+    form.addEventListener(
+        'submit',
+        function(){
+            event.preventDefault();
+            request("https://api.mercadolibre.com/sites/MLA/search?q=" + input.value,
+                function(responseText){
+                    //TODO: Hacer un refactor para crear un template de hbs.
+                    var response = JSON.parse(responseText);
+                    var breadcrumbsSource = document.querySelector('#breadcrumbs').innerHTML;
+                    var breadcrumbsTemplate = Handlebars.compile(breadcrumbsSource);
+                    var breadcrumbsContext = {
+                        breadcrumbs: response.filters[0].values[0].path_from_root,
+                        query: input.value
+                    };
+                    var breadcrumbsHTML = breadcrumbsTemplate(breadcrumbsContext);
+                    var itemListSource = document.querySelector("#item-list").innerHTML;
+                    var itemListTemplate = Handlebars.compile(itemListSource);
+                    var itemListContext = {
+                        items: response.results
+                    };
+                    var itemListHTML = itemListTemplate(itemListContext);
+                    document.querySelector("main").innerHTML = breadcrumbsHTML + itemListHTML;
+        },
+        function () {
 
+        });
+    });
 }
 
 //Use this method to make request to the API
