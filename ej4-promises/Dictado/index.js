c488e87d3afb1a0c3f1581f5abed24cfff431038
sebaslewis <sebastian.lewis@mercadolibre.com>
2017-10-11 09:57:55 -0300

    Read file with callback in promises exercise

--- ej4-promises/Dictado/index.js
+++ ej4-promises/Dictado/index.js
@@ -1,7 +1,17 @@
+let fs = require('fs');
 let readFileWithPromise = require('./readFileWithPromise');
 
+console.log('Leyendo el archivo con callback\n');
+fs.readFile('./trabalenguas.txt', 'utf8', (err, data) => {
+  if(err) return console.error('Error al leer el archivo ' + err);
+  console.log(data);
+});
+console.log('Fin de leer el archivo con callback\n');
+
+console.log('Leyendo el archivo con promise\n');
 readFileWithPromise('./trabalenguas.txt').then(res => {
     console.log(res);
 }).catch(err => {
     console.error('Error al leer el archivo ' + err);
 })
+console.log('Fin de leer el archivo con promise\n');
