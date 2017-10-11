ac56ec678d2e02d5417d8e1aba0c20c69bea891c
lucarrizo <lucas.carrizo@mercadolibre.com>
2017-10-11 12:24:06 -0300

    add promise

---
+++ ej4-promises/requestPromise.js
@@ -0,0 +1,33 @@
+const https = require('https')
+const http = require('http');
+
+module.exports = (options) => {
+
+    return new Promise((resolve, reject) => {
+
+        const protocol = (options.protocol === 'https') ? https : http;
+        delete options.protocol;
+
+        protocol.request(options, (resp) => {
+            let data = '';
+
+            resp.on('data', chunck => {
+                data += chunck;
+            });
+
+            resp.on('end', () => {
+                data = JSON.parse(data);
+                //handle http errors
+                if (resp.statusCode < 200 || resp.statusCode > 299) {
+                    reject(data);
+                } else {
+                    resolve(data);
+                }
+            });
+        }).on('error', (err) => {
+            reject({
+                err
+            });
+        }).end();
+    })
+};
