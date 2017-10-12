8ebce97d8462b78eca8f31832f9b7954d6715f94
Facundo Allemand <facundo.allemand@mercadolibre.com>
2017-10-12 17:52:07 -0300

    update resolution

--- ej3-request/resolution/request.js
+++ ej3-request/resolution/request.js
@@ -1,23 +1,30 @@
-module.exports = (options, callback) => {
-  var protocol = (options.protocol === 'http') ? require('http') : require('https');
-  delete options.protocol;
-  protocol.request(options, response => {
-    var result = [];
-    response.on('data', chunk => {
-      result.push(chunk);
-    })
-    response.on('end', () => {
-      result = JSON.parse(result.join(''));
-      // Handle http errors
-      if (response.statusCode < 200 || response.statusCode > 299) {
-        callback(result);
-      }
-      else {
-        callback(result);
-      }
-    })
+const https = require('https');
+const http = require('http');
 
-  }).on('error', function(err) {
-    callback(err);
-  }).end();
-}
+module.exports = (options, success, error) => {
+
+    const protocol = (options.protocol == 'http') ? http : https;
+    delete options.protocol;
+
+    protocol.request(options, response => {
+        let result = '';
+
+        response.on('data', chunk => {
+            result += chunk;
+        });
+
+        response.on('end', () => {
+            result = JSON.parse(result);
+            // Handle http errors
+            if (response.statusCode < 200 || response.statusCode > 299) {
+                success(result);
+            }
+            else {
+                error(result);
+            }
+        })
+
+    }).on('error', function(err) {
+        error(err);
+    }).end();
+};
