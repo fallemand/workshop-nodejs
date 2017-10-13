482ac78e7fdd6310a8ad714a1546107645ea787c
Gonzalo E. Rodriguez Isleño <gonzalo.rodriguez@mercadolibre.com>
2017-10-13 10:44:00 -0300

    Splitted solutions from second class.

--- ej3-request/request.js
+++ ej3-request/request.js
@@ -1,28 +1,27 @@
 const https = require('https');
 const http = require('http');
 
-module.exports = (options) => {
-    return new Promise ( (resolve, reject) => {
-        const protocol = (options.protocol === 'http') ? http : https;
-        delete options.protocol;
+module.exports = (options, callback) => {
+    const protocol = (options.protocol === 'http') ? http : https;
+    delete options.protocol;
 
-        protocol.request(options, (res) => {
-            let data = '';
+    protocol.request(options, (res) => {
+        let data = '';
 
-            res.on('data', (chunk) => {
-                data += chunk;
-            });
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
 
-            res.on('end', () => {
-                data = JSON.parse(data);
-                if (res.statusCode < 200 || res.statusCode > 299) {
-                    reject(res.statusCode);
-                }
-                resolve(data);
-            });
-        }).on('error', (err) => {
-            reject(err);
-        }).end();
-    });
+        res.on('end', () => {
+            data = JSON.parse(data);
+            if (res.statusCode < 200 || res.statusCode > 299) {
+                callback(err,null)
+            }
+            callback(null,data);
+        })
+    }).on('error', (err) => {
+        console.log(err);
+        callback(err,null);
+    }).end();
 };
 
