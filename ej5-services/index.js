14a90f345e71e7ccc84b817ebad569f823721ba6
Gonzalo E. Rodriguez Isleño <gonzalo.rodriguez@mercadolibre.com>
2017-10-13 12:28:46 -0300

    Third class. Fifth exercise.

---
+++ ej5-services/index.js
@@ -0,0 +1,34 @@
+const meliService = require('./meli.service');
+
+console.log('-------------- ITEM ----------------');
+
+meliService.item('MLA659477272')
+  .then( (data) => {
+    console.log(data);
+
+    console.log('-------------- SEARCH ----------------');
+
+    meliService.search('2312312321')
+      .then( (data) => {
+        console.log(data);
+
+        console.log('-------------- SUGGEST ----------------');
+
+        meliService.suggest('3123213')
+          .then( (data) => {
+            console.log(data);
+          }).catch( (err) => {
+          console.log(err);
+        });
+
+      }).catch( (err) => {
+      console.log(err);
+    });
+  }).catch( (err) => {
+    console.log(err);
+});
+
+
+
+
+
