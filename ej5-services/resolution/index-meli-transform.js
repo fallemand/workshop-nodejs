b9751e1beb075816cbfe3db9d1759194aefb9539
rodracastro <rodrigo.castro@mercadolibre.com>
2017-10-20 10:22:53 -0300

    Added meli transform service

---
+++ ej5-services/resolution/index-meli-transform.js
@@ -0,0 +1,43 @@
+const meliService = require('../resolution/meli.transform');
+
+//*** VIP with callback ***
+meliService.item('MLA632591345', (item) => {
+  console.log("*** VIP with callback ***");
+  console.log(item);
+});
+
+//*** Item Search with callback ***
+meliService.search('iphone', 'MLA', (results) => {
+  console.log("*** Item Search with callback ***");
+  console.log(results);
+});
+
+//*** Item suggestion with callback ***
+meliService.suggest('iph', 'MLA', (results) => {
+  console.log("*** Item suggestion with callback ***");
+  console.log(results);
+});
+
+//*** VIP with promise ***
+meliService.itemWithPromise('MLA632591345').then((data) => {
+  console.log("*** VIP with promise ***");
+  console.log(data);
+}).catch((err) => {
+  console.log(err);
+});
+
+//!*** Item Search with promise ***
+meliService.searchWithPromise('iphone', 'MLA').then((data) => {
+  console.log("*** Item Search with promise ***");
+  console.log(data);
+}).catch((err) => {
+  console.log(err);
+});
+
+//*** Item suggestion with promise ***
+meliService.suggestWithPromise('iphone', 'MLA').then((data) => {
+  console.log("*** Item suggestion with promise ***");
+  console.log(data);
+}).catch((err) => {
+  console.log(err);
+});
