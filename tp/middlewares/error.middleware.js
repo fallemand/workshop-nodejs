7e72d1c3cf7b39b15e039fbd324da6b4e082353e
Luciano Battagliero <lucianobattagliero+git@gmail.com>
2017-10-25 12:09:58 -0300

    Add middleware placeholder method

--- tp/middlewares/error.middleware.js
+++ tp/middlewares/error.middleware.js
@@ -1,6 +1,10 @@
 const ErrorMiddleware = {
   apiError(err, req, res, next) {
     res.status(err.status || 500).json({ status: err.status, message: err });
+  },
+
+  appError(err, req, res, next) {
+    // TODO: implement!
   }
 };
 
