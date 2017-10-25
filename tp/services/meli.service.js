c10aeb3fa31a5a503fa7aa35c7481cacc6a88f6c
Luciano Battagliero <lucianobattagliero+git@gmail.com>
2017-10-25 10:48:39 -0300

    Add services

---
+++ tp/services/meli.service.js
@@ -0,0 +1,67 @@
+const request = require('./request');
+const transform = require('./meli.transform');
+
+const options = {
+  method: 'GET',
+  headers: {
+    'Content-type': 'application/json',
+  },
+  hostname: 'api.mercadolibre.com',
+  path: ''
+};
+const protocol = 'https';
+
+class MeliService {
+  static search(query, callback) {
+    options.path = `/sites/MLA/search?q=${query}`;
+
+    return new Promise((resolve, reject) => {
+      return request(options, protocol)
+        .then((res) => resolve(transform.results(res)))
+        .catch((err) => reject(err));
+    });
+  }
+
+  static suggest(query, callback) {
+    options.hostname = 'http2.mlstatic.com';
+    options.path = `/resources/sites/MLA/autosuggest?q=${query}`;
+
+    return request(options, protocol);
+  }
+
+  // TODO: calls to Currency and User.
+  static item(id, callback) {
+    let data;
+
+    const getItem = (id) => {
+      options.path = `/items/${id}`;
+
+      return request(options, protocol);
+    };
+
+    const getDesc = (id) => {
+      options.path = `/items/${id}/description`;
+
+      return request(options, protocol);
+    };
+
+    const getCateg = (id) => {
+      options.path = `/categories/${id}`;
+
+      return request(options, protocol);
+    };
+
+    return Promise.all([
+      getItem(id),
+      getDesc(id)
+    ])
+    .then(
+      (ItemAndDescRes) => getCateg(ItemAndDescRes[0].category_id)
+        .then((CategRes) => transform.item(ItemAndDescRes[0], ItemAndDescRes[1], CategRes))
+        .catch((err) => err)
+      )
+    .catch((err) => err);
+  }
+}
+
+module.exports = MeliService;
