cc8c91b202501c2339244c0439432e5ded51a7b0
Facundo Allemand <facundo.allemand@mercadolibre.com>
2017-10-31 10:28:49 -0300

    update resolution

--- tp/parts/2-express/f.react/resolution/routers/api.router.js
+++ tp/parts/2-express/f.react/resolution/routers/api.router.js
@@ -4,6 +4,6 @@ const controller = require('../controllers/api.controller');
 
 router.get('/items/:id', controller.items);
 router.get('/search', controller.search);
-router.get('/suggest', controller.suggest)
+router.get('/suggest', controller.suggest);
 
 module.exports = router;
