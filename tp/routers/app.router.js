6b0871d8308d7c636d7be09e35496fa0ea1b0194
Luciano Battagliero <lucianobattagliero+git@gmail.com>
2017-11-03 10:45:57 -0300

    Fix app router

--- tp/routers/app.router.js
+++ tp/routers/app.router.js
@@ -3,6 +3,6 @@ const controller = require('../controllers/app.controller');
 
 router.get('/', controller.main);
 router.get('/search', controller.search);
-router.get('/items/:id', controller.search);
+router.get('/items/:id', controller.items);
 
 module.exports = router;
