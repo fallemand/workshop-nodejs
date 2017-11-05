0c922b3a9ce4df3348ffb302a3e5116b56d45992
Facundo Allemand <facundo.allemand@mercadolibre.com>
2017-11-05 15:08:52 -0300

    update test.

--- tp/dictado/test/unit/views/suggest.spec.js
+++ tp/dictado/test/unit/views/suggest.spec.js
@@ -4,7 +4,24 @@ const Suggest = require('../../../views/components/Suggest');
 
 describe('Suggest (Snapshot)', () => {
   it('Suggest renders', () => {
-    const component = renderer.create(<Suggest query="iph" />);
+
+    const suggestResponse = {
+      "query": "iphone",
+      "results": [
+        "iphone",
+        "iphone 6",
+        "iphone 7",
+      ]
+    };
+
+    // Mock window.fetch
+    window.fetch = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(suggestResponse)
+      })
+    );
+
+    const component = renderer.create(<Suggest query="iph"/>);
     const json = component.toJSON();
     expect(json).toMatchSnapshot();
   });
