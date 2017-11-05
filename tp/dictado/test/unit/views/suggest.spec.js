3ae4be156ba3add1e05cb446b3073082077c1a44
Facundo Allemand <facundo.allemand@mercadolibre.com>
2017-11-05 20:30:53 -0300

    unit test suggest

--- tp/dictado/test/unit/views/suggest.spec.js
+++ tp/dictado/test/unit/views/suggest.spec.js
@@ -4,19 +4,30 @@ const Suggest = require('../../../views/components/Suggest');
 
 describe('Suggest (Snapshot)', () => {
   const component = renderer.create(<Suggest query="iph"/>);
+  const suggestResponse = {
+    "results": [
+      "iphone 6",
+      "iphone 7",
+      "iphone 5",
+    ],
+  };
 
   it('Suggest renders without results', () => {
     const json = component.toJSON();
     expect(json).toMatchSnapshot();
   });
 
-  it('Suggest renders results', () => {
+  it('Suggest renders resultssuggestResponse [change state]', () => {
     const instance = component.getInstance();
-    instance.setState({ results: [
-      "iphone",
-      "iphone 6",
-      "iphone 7",
-    ]});
+    instance.setState(suggestResponse);
+    const json = component.toJSON();
+    expect(json).toMatchSnapshot();
+  });
+
+  it('Suggest renders results [call componentWillReceiveProps]', () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({json: () => Promise.resolve(suggestResponse)}));
+    const instance = component.getInstance();
+    instance.componentWillReceiveProps({query: 'iph'});
     const json = component.toJSON();
     expect(json).toMatchSnapshot();
   });
