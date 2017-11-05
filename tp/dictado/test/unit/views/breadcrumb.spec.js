1d8fa9c1d9d90c0d1b6b911f516fdec637c93403
Facundo Allemand <facundo.allemand@mercadolibre.com>
2017-11-05 13:15:07 -0300

    add category

--- tp/dictado/test/unit/views/breadcrumb.spec.js
+++ tp/dictado/test/unit/views/breadcrumb.spec.js
@@ -2,15 +2,21 @@ const React = require('react');
 const renderer = require('react-test-renderer');
 const Breadcrumb = require('../../../views/components/Breadcrumb');
 
+const category = [
+  {'id': '1', 'name': 'Celulares y Telefonía'},
+  {'id': '2','name': 'Celulares'},
+  {'id': '3', 'name': 'Iphone'}
+];
+
 describe('Breadcrumb (Snapshot)', () => {
   it('Breadcrumb renders item', () => {
-    const component = renderer.create(<Breadcrumb category={[{'id': '1', 'name': 'Celulares y Telefonía'}, {'id': '2','name': 'Celulares'}, {'id': '3', 'name': 'Iphone'}]} />);
+    const component = renderer.create(<Breadcrumb category={category} />);
     const json = component.toJSON();
     expect(json).toMatchSnapshot();
   });
 
   it('Breadcrumb renders search', () => {
-    const component = renderer.create(<Breadcrumb category={[{'id': '1', 'name': 'Celulares y Telefonía'}, {'id': '2', 'name': 'Celulares'}, {'id': '3', 'name': 'Iphone'}]} query="iphone 7" />);
+    const component = renderer.create(<Breadcrumb category={category} query="iphone 7" />);
     const json = component.toJSON();
     expect(json).toMatchSnapshot();
   });
