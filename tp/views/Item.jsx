2977d5d4d75ad9482c194ebfe5fb591fc059200f
Luciano Battagliero <lucianobattagliero+git@gmail.com>
2017-11-08 11:49:18 -0300

    Update Item view

--- tp/views/Item.jsx
+++ tp/views/Item.jsx
@@ -3,13 +3,52 @@ const Layout = require('./components/Layout');
 
 class Item extends React.Component {
   render() {
-    // const {
-    //   results,
-    // } = this.props;
+    const {
+      title,
+      price,
+      condition,
+      sold_quantity,
+      free_shipping,
+      picture,
+      description,
+      categories,
+    } = this.props.item.data;
 
     return (
       <Layout {...this.props}>
-        ITEM
+        <div className="item">
+          <div className="item__picture">
+            <img src={picture} alt={title} />
+          </div>
+          <div className="item__info">
+            <span className="item__condition">
+              {(condition === 'new') ? 'Nuevo' : 'Usado'}
+              -
+              {sold_quantity} vendidos
+              {
+                free_shipping &&
+                <i className="item__freeshipment"></i>
+              }
+            </span>
+            <span className="item__title">
+              {title}
+            </span>
+            <span className="item__price">
+              $ {price.amount}
+            </span>
+            <a className="item__buy" href="#buy">
+              Comprar
+            </a>
+          </div>
+          <div className="item__description">
+            <h2 className="item__description-title">
+              Descripción del producto
+            </h2>
+            <div className="item__description-content">
+              {description}
+            </div>
+          </div>
+        </div>
       </Layout>
     );
   }
