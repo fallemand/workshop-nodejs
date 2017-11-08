8b9c64a92c3208cc26b32990c606aa98bf039c5f
Luciano Battagliero <lucianobattagliero+git@gmail.com>
2017-11-08 11:54:09 -0300

    Update Header view

--- tp/views/components/Header.jsx
+++ tp/views/components/Header.jsx
@@ -4,15 +4,15 @@ class Header extends React.Component {
   render() {
     return (
       <div className="header__container">
-          <a className="header__logo" href="/" tabIndex="1">
+          <a className="header__logo" href="/app/" tabIndex="1">
             MercadoLibre - Donde compras y vendes de todo
           </a>
           <form className="header__search" action="/app/search" method="GET" role="search">
               <input type="text" className="header__search-input" name="q" max-length="120"
                 tabIndex="2" autoCapitalize="off" autoComplete="off" autoCorrect="off"
                 spellCheck="false" placeholder="Nunca dejes de buscar"
-                data-js="search" value="iphone" />
-              <button type="submit" className="header__search-btn" data-fastclick="" tabIndex="3">
+                data-js="search" value="" />
+              <button type="submit" className="header__search-btn" tabIndex="3">
                 <i className="header__search-icon">
                   <span>Buscar</span>
                 </i>
