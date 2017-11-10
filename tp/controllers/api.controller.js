16f861ce937cc4ddd215cdcf4aea502bb0cac968
ekohmann <emiliano.kohmann@mercadolibre.com>
2017-11-10 09:36:24 -0300

    Testing

--- tp/controllers/api.controller.js
+++ tp/controllers/api.controller.js
@@ -2,41 +2,41 @@ const meliService = require('../services/meli.service')
 
 class ApiController {
 
-    static search(req, res, next) {
-        const site = req.params.site;
-        const q = req.query.q;
+  static search(req, res, next) {
+    const site = req.params.site;
+    const q = encodeURIComponent(req.query.q);
 
-        meliService.searchWithPromise(q, site).then((result) => {
-            result.author = res.locals.author;
-            res.json(result);
-        }).catch(next);
-    };
-    
-    static suggest(req, res, next) {
-        const site = req.params.site;
-        const q = req.query.q;
+    meliService.searchWithPromise(q, site).then((result) => {
+      result.author = res.locals.author;
+      res.json(result);
+    }).catch(next);
+  };
 
-        meliService.suggestWithPromise(q, site).then((result) => {
-            result.author = res.locals.author;
-            res.json(result);
-        }).catch(next);
-    };
-    
-    static items(req, res, next) {
-        const itemId = req.params.id;
+  static suggest(req, res, next) {
+    const site = req.params.site;
+    const q = encodeURIComponent(req.query.q);
 
-        meliService.itemWithPromise(itemId).then((result) => {
-            result.author = res.locals.author;            
-            res.send(result);
-        }).catch(next);
-    };
-    
-    /*
-    static itemsPost(req, res) {
-        const params = req.params;
-        res.send(params);
-    };
-    */
+    meliService.suggestWithPromise(q, site).then((result) => {
+      result.author = res.locals.author;
+      res.json(result);
+    }).catch(next);
+  };
+
+  static items(req, res, next) {
+    const itemId = req.params.id;
+
+    meliService.itemWithPromise(itemId).then((result) => {
+      result.author = res.locals.author;
+      res.send(result);
+    }).catch(next);
+  };
+
+  /*
+  static itemsPost(req, res) {
+      const params = req.params;
+      res.send(params);
+  };
+  */
 }
 
 module.exports = ApiController;
--- tp/controllers/app.controller.js
+++ tp/controllers/app.controller.js
@@ -2,61 +2,65 @@ const meliService = require('../services/meli.service');
 const request = require('../services/requestPromise');
 
 class AppController {
-    
-    static search(req, res, next) {
-        const site = req.params.site;
-        const q = req.query.q;
-        const url = `/api/search/${site}?q=${q}`;
 
-        const options = {
-            method: 'GET',
-            headers: {'Content-type': 'application/json'},
-            hostname: 'localhost',
-            port: '3000',
-            protocol: 'http',
-            path: url
-        };
+  static search(req, res, next) {
+    const site = req.params.site;
+    const q = req.query.q;
+    const url = `/api/search/${site}?q=${encodeURIComponent(q)}`;
 
-        request(options).then((results) => {
-            results.query = q;
-            res.render('Search', results);
-        }).catch(next);
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      hostname: 'localhost',
+      port: '3000',
+      protocol: 'http',
+      path: url
     };
-    
-    static items(req, res, next) {
-        const itemId = req.params.id;
 
-        const url = `/api/items/${itemId}`;
-        
-        const options = {
-            method: 'GET',
-            headers: {'Content-type': 'application/json'},
-            hostname: 'localhost',
-            port: '3000',
-            protocol: 'http',
-            path: url
-        };
+    request(options).then((results) => {
+      results.query = q;
+      res.render('Search', results);
+    }).catch(next);
+  };
 
-        request(options).then((results) => {
-            res.render('Item', results);
-        }).catch(next);
+  static items(req, res, next) {
+    const itemId = req.params.id;
+
+    const url = `/api/items/${itemId}`;
+
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      hostname: 'localhost',
+      port: '3000',
+      protocol: 'http',
+      path: url
     };
-    
-    static test(req, res, next) {
-        res.render('IndexEmi', {
-            title: 'Titulo!',
-            tasks: [
-                'task1',
-                'task2',
-                'task3'
-            ]
-            //subtitle: 'Subtitulo!'
-        });
-    };
-        
-    static index(req, res, next) {
-        res.render('Index');
-    };
+
+    request(options).then((results) => {
+      res.render('Item', results);
+    }).catch(next);
+  };
+
+  static test(req, res, next) {
+    res.render('IndexEmi', {
+      title: 'Titulo!',
+      tasks: [
+          'task1',
+          'task2',
+          'task3'
+        ]
+        //subtitle: 'Subtitulo!'
+    });
+  };
+
+  static index(req, res, next) {
+    res.render('Index');
+  };
 }
 
 module.exports = AppController;
