f2c40ce809e0fd5dab9b9a04767d2378e4c8e227
Ariel Iván Mariani <ariel.mariani@mercadolibre.com>
2017-11-13 07:45:03 -0300

    Fix suggest test

--- tp/test/unit/suggest.spec.js
+++ tp/test/unit/suggest.spec.js
@@ -4,6 +4,16 @@ const Suggest = require('../../views/components/Suggest');
 
 describe('Suggest (Snapshot)', () => {
   const component = renderer.create(<Suggest query="iph" />);
+  const suggestedResponse = {
+    results: ['iphone', 'iphone 2', 'iphone 3'],
+  };
+
+  afterEach(() => {
+    const instance = component.getInstance();
+    instance.setState({
+      results: [],
+    });
+  });
 
   test('Suggest renders without results', () => {
     const json = component.toJSON();
@@ -12,32 +22,21 @@ describe('Suggest (Snapshot)', () => {
 
   test('Suggest renders with results [Change State]', () => {
     const instance = component.getInstance();
-
-    instance.setState({
-      results: ['iphone', 'iphone 2', 'iphone 3'],
-    });
-
+    instance.setState(suggestedResponse);
     const json = component.toJSON();
     expect(json).toMatchSnapshot();
   });
 
   test('Suggest renders with results [componentWillReceiveProps]', () => {
+    window.fetch = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve({ json: () => Promise.resolve(suggestedResponse) }));
+    // Tell jest to expect something asyncronous
+    expect.assertions(1);
     const instance = component.getInstance();
-    const response = {
-      results: ['iphone', 'iphone 5'],
-    };
-
-    window.fetch = jest.fn().mockImplementation(() =>
-      Promise.resolve({
-        json: () => {
-          Promise.resolve(response);
-        },
-      })
-    );
-
-    instance.componentWillReceiveProps({ query: 'iph' });
-
-    const json = component.toJSON();
-    expect(json).toMatchSnapshot();
+    return instance.componentWillReceiveProps({ query: 'iph' }).then(() => {
+      const json = component.toJSON();
+      expect(json).toMatchSnapshot();
+    });
   });
 });
