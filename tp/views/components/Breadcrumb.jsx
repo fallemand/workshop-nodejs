6695c47427371c139ce95e292831926d5f73a0f8
abrana <agustin.brana@mercadolibre.com>
2017-11-13 12:29:37 -0300

    Remove unnecessary tag a

--- tp/views/components/Breadcrumb.jsx
+++ tp/views/components/Breadcrumb.jsx
@@ -5,9 +5,9 @@ class Breadcrumb extends React.Component {
         const { category, query } = this.props;
        return (
             <ul class="breadcrumb">
-                <li class="breadcrumb__element"><a href="/">Inicio</a></li>
+                <li class="breadcrumb__element"><a href="/app">Inicio</a></li>
                 {category && category.map((category) =>
-                    <li class="breadcrumb__element"><a href="/">{category.name}</a></li>
+                    <li class="breadcrumb__element">{category.name}</li>
                 )}
                 
               {query && <li class="breadcrumb__element">"{query}"</li>}
