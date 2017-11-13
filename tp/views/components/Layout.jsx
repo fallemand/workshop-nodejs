8fc599d23cc973e9422e5ce9e7b72a611fc2e659
Luciano Battagliero <lucianobattagliero+git@gmail.com>
2017-11-13 10:46:06 -0300

    Update layout component

--- tp/views/components/Layout.jsx
+++ tp/views/components/Layout.jsx
@@ -6,30 +6,29 @@ class Layout extends React.Component {
   render() {
     return (
       <html>
-          <head>
-            <meta charSet="utf-8"/>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
-            <meta httpEquiv="cleartype" content="on"/>
-            <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
-            <meta name="theme-color" content="#fff059"/>
-            <title>Mercado Libre | FrontEnd Test</title>
-            <link rel="shortcut icon" href="http://ui.mlstatic.com/navigation/1.2.0/mercadolibre/favicon.ico"/>
-            <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400" rel="stylesheet" />
-            <link href="/assets/dist/styles/main.css" rel="stylesheet" />
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+          <meta httpEquiv="cleartype" content="on" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="theme-color" content="#fff059" />
+          <title>Mercado Libre | FrontEnd Test</title>
+          <link rel="shortcut icon" href="http://ui.mlstatic.com/navigation/1.2.0/mercadolibre/favicon.ico" />
+          <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400" rel="stylesheet" />
+          <link href="/assets/dist/styles/main.css" rel="stylesheet" />
         </head>
         <body>
-            <header role="banner" id="header" className="header">
-              <Header query={this.props.query} />
-            </header>
-            <main role="main" className="main">
-                <Breadcrumb {...this.props} />
-                <div className="main__content">
-                  {this.props.children}
-                </div>
-            </main>
-            <script src="/statics/dist/scripts/main.js"></script>
+          <header role="banner" id="header" className="header" data-js="header">
+            <Header query={this.props.query} />
+          </header>
+          <main role="main" className="main">
+            <Breadcrumb query={this.props.query} categories={this.props.data && this.props.data.categories} />
+            <div className="main__content">
+              {this.props.children}
+            </div>
+          </main>
         </body>
-      </html>
+    </html>
     );
   }
 }
