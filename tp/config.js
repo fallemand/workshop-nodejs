07aa1d295887708c64940795bdfd3baca62598c5
Ariel Iván Mariani <ariel.mariani@mercadolibre.com>
2017-11-16 09:35:57 -0300

    Added fallback for port and host on production.

--- tp/config.js
+++ tp/config.js
@@ -1,6 +1,6 @@
 const production = {
-  host: process.env.IP,
-  port: process.env.PORT,
+  host: process.env.IP || 'localhost',
+  port: process.env.PORT || '3000',
   useMocks: false,
 };
 const development = {
