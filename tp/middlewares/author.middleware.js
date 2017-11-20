0679457b14fd55e628c333e26730475e1f361a49
jessipedernera <jessica.pedernera@mercadolibre.com>
2017-11-20 20:05:43 -0300

    adding readme

--- tp/middlewares/author.middleware.js
+++ tp/middlewares/author.middleware.js
@@ -1,3 +1,12 @@
+/*Middleware --> funcion de 3 parametros: request, response, next
+* Cosas comunes para las diferentes rutas 
+* Podes decir en que orden debe ejecutarse
+* Está entre el request y el response
+* EJ: que valide al user, que se meta antes de todas las llamadas, antes de algunas llamadas
+* En express everything es un middleware
+* Los middleware de error tienen un parametro mas: Primero el de error.
+* */
+
 module.exports = (req, res, next) => {
   res.locals.author = {
     name: 'Jess Pedernera',
