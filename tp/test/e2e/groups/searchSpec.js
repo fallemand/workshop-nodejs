711ed8cfe3534975be7d5a934fc3a81f5e5b9a50
jessipedernera <jessica.pedernera@mercadolibre.com>
2017-11-20 22:54:39 -0300

    Functional Tests

--- tp/test/e2e/groups/searchSpec.js
+++ tp/test/e2e/groups/searchSpec.js
@@ -1,15 +1,30 @@
-searchCellphone = (client, query) => {
+searchQuery = (client, query) => {
   const homePage = client.page.HomePage();
   homePage.search(query);
 
   const searchPage = client.page.SearchPage();
   searchPage.waitForElementVisible('@breadcrumb', 2000)
-    .assert.containsText('@breadcrumb', '"' + query + '"');
+  .assert.containsText('@breadcrumb', '' + query + '')
+  .assert.visible('@firstElement');
+};
+
+selectFirstItem = (client) => {
+  const searchPage = client.page.SearchPage();
+  searchPage.selectItem();
+
+  const itemPage = client.page.VipPage();
+  itemPage.waitForElementVisible('@nameItem', 2000)
+  .assert.visible('@priceItem')
+  .assert.visible('@imageItem')
+  .assert.visible('@descriptionItem')
+  .assert.containsText('@nameItem', 'Set 10 Anillos Hippies Boho Middle Ring India Elefante Hindú');
 };
 
 module.exports = {
-  'Should search Iphone': (client) => {
-    searchCellphone(client, 'iphone')
+  'Should search Anillos': (client) => {
+    searchQuery(client, 'anillos')
+    selectFirstItem(client)
+    searchQuery(client, 'pulseras')
   },
   beforeEach: function (client) {
     client.url(client.globals.site);
---
+++ tp/test/e2e/pages/VipPage.js
@@ -0,0 +1,8 @@
+module.exports = {
+    elements: {
+        'nameItem': 'span.item__title',
+        'priceItem': 'span.item__price',
+        'imageItem': '.item__picture img',
+        'descriptionItem': '.item__description-content'
+    }
+};
