ec5bb8c789cb03698c81609d96448441ff9ff676
jessipedernera <jessica.pedernera@mercadolibre.com>
2017-11-20 21:15:40 -0300

    [UT]: meliService, suggest

--- tp/test/unit/meli.service.spec.js
+++ tp/test/unit/meli.service.spec.js
@@ -1,23 +1,24 @@
 const meliService = require('../../services/meli.service');
 require('../../mocks');
 
-describe('Test meli.service.js',() => {
-    test ('It should search',() => {
-        return meliService.search('Anillos').then((response) => {
-            expect(response.results).toBeDefined();
-            expect(response.results.length).toBeGreaterThan(4);
-            expect(response.results[0].title).toBeDefined();
-            expect(response.results[0].price).toBeDefined();
-            expect(response.category).toBeDefined();
-        });
-    });
+describe('meli.service', () => {
+  it('should search items', () => {
+    return meliService.search('Anillos').then(result => {
+      expect(result.results).toBeDefined();
+      expect(result.results.length).toBeGreaterThan(2);
+      expect(result.results[0].title).toBeDefined();
+      expect(result.results[0].price).toBeDefined();
+      expect(result.results[0].category_id).toBeDefined();
+    })
+  });
 
-    test ('It should get item',() => {
-        return meliService.item('MLA1281119').then((item) => {
-            expect(item.id).toBeDefined();
-            expect(item.category).toBeDefined();
-            expect(item.results.length).toBeGreaterThan(2);
-            expect(item.picture).toEqual('url');
-        });
-    });
+  it('should get item', () => {
+    return meliService.item('MLA640761245').then(item => {
+        expect(item.id).toBeDefined();
+        expect(item.title).toEqual('Anillos Acero Quirurgico Combo X Mayor Lote 12 Unidades');
+        expect(item.category).toBeDefined();
+        expect(item.description).toBeDefined();
+        expect(item.description).toEqual('LEER ANTES DE OFERTAR Material: Aleación Metálica Color:Plateado o Rose Gold Medida:17-18-19mm Precio unitario (1 Unidad) Consultar stock antes de ofertar De ser necesario elegir COLOR, MODELO, TALLE, ETC y no encontrarse la opción detallada al comprar enviar por chat de MERCADOLIBRE el articulo elegido, de lo contrario se enviara al azar Una vez realizada la compra se manejara la compra por chat o Mail, NO TELEFONICAMENTE!! Formas de Pago: Solo MERCADOPAGO, MERCADOLIBRE desactivo la opción pago en efectivo para productos nuevos Formas de Envío: Mercadoenvios Consultá el costo de envío en la publicación con código postal Los tiempos de envío publicitados son pura responsabilidad del correo elegido y son estimativos no exactos (PUEDEN SUFRIR DEMORAS) Compras por Mercado Libre son solo con mercadoenvios, si queres retirar debes hacer la compra por otro medio. No realizamos envíos consolidados ya que de esa forma no va el paquete asegurado si se pierde o pasa algo en el camino. Si querés comprar por mercadolibre tenés que abonar envío para cada artículo. Sino deberías hacerlo por otro medio para que se pueda enviar todo junto abonando solo un envio ---------------------------------------------------------------------- ¿Qué es Aleación Metálica? Una aleación es una combinación de propiedades metálicas, que está compuesta de dos o más elementos metálicos sólidos. Las aleaciones están constituidas por elementos metálicos como Zinc, Hierro, Cobre, etc. Los productos son importados por lo que no podemos darte una composición de que metales contiene. Soy alérgica. ¿Puedo usar sus accesorios? No podemos dar una respuesta a esta pregunta ya que cada persona es diferente. Correcto Cuidados de los Accesorios No Mojar No rociar perfume o cualquier elemento que contenga alcohol. No guardar en lugares húmedos. (Por Ej: Baño) Extrema sudoración puede causar decoloración de los accesorios. Limpiar solo con paño seco BELLA BIJOUTERIE');
+    })
+  });
 });
--- tp/test/unit/suggest.spec.js
+++ tp/test/unit/suggest.spec.js
@@ -3,46 +3,42 @@ const renderer = require('react-test-renderer');
 const Suggest = require('../../views/components/Suggest');
 
 describe('Suggest (Snapshot)', () => {
-    // Metodo de testeo, útil para componentes
-    const component = renderer.create(<Suggest query="iph" />);
-    const response = {suggested_queries: [
-        {q: 'iphone'},
-        {q: 'iphone 5'},
-        {q: 'iphone 6'}
-    ]};
+  const component = renderer.create(<Suggest query="anil"/>);
+  const suggestResponse = {
+    "results": [
+      "anillos",
+      "anillos oro",
+      "anillos corona",
+    ],
+  };
 
-    afterEach(() => {
-        const instance = component.getInstance();
-        instance.setState(() => {
-            results: []
-        });
+  afterEach(() => {
+    // Clean component state
+    const instance = component.getInstance();
+    instance.setState({
+      results: []
     });
+  });
 
-    test('Suggest render without results',()=> {
-        const json = component.toJSON();
-        expect(json).toMatchSnapshot();
-    });
+  test('Suggest renders resultssuggestResponse [change state]', () => {
+    const instance = component.getInstance();
+    instance.setState(suggestResponse);
+    const json = component.toJSON();
+    expect(json).toMatchSnapshot();
+  });
 
-    test('Suggest render with results [change state]',()=> {
-        const instance = component.getInstance();
-        instance.setState({
-            suggested_queries: [{ q: 'iphone', q: 'iphone 6'}]
-        });
-        const json = component.toJSON();
-        expect(json).toMatchSnapshot();
-    });
+  test('Suggest renders without results', () => {
+    const json = component.toJSON();
+    expect(json).toMatchSnapshot();
+  });
 
-    test('Suggest render with results [componentWillReceiveProps]',()=> {
-        const instance = component.getInstance();
-        window.fetch = jest.fn().mockImplementation(
-            () => Promise.resolve({json: () => Promise.resolve({
-                suggested_queries: [{ q: 'iphone', q: 'iphone 6'}]
-            })})
-        );
-        expect.assertions(1);
-        return instance.componentWillReceiveProps({query: 'iph'}).then(() => {
-            const json = component.toJSON();
-            expect(json).toMatchSnapshot();
-        });
+  test('Suggest renders results [call componentWillReceiveProps]', () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({json: () => Promise.resolve(suggestResponse)}));
+    expect.assertions(1);
+    const instance = component.getInstance();
+    return instance.componentWillReceiveProps({query: 'anil'}).then(() => {
+      const json = component.toJSON();
+      expect(json).toMatchSnapshot();
     });
+  });
 });
